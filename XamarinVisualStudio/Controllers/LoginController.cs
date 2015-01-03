3eb66121ff436b3ffee8ea5f8f6108807325c479
Sunil <sbshetty@opteamix.com>
2015-01-03 14:58:41 +0530

    Packages added and zipping code added

--- XamarinVisualStudio/Controllers/LoginController.cs
+++ XamarinVisualStudio/Controllers/LoginController.cs
@@ -8,7 +8,9 @@ using BizLogin = XamarinVisualStudio.Biz.Data;
 using XamarinVisualStudio.Biz;
 using AutoMapper;
 using ModelLogin = XamarinVisualStudio.Models;
-
+using System.IO;
+using System.IO.Compression;
+using System.Text;
 
 namespace XamarinVisualStudio.Controllers
 {
@@ -61,10 +63,18 @@ namespace XamarinVisualStudio.Controllers
 				return View("Login");
 		}
 		[HttpPost]
-		public ActionResult AuthorLogin()
+		public ActionResult AuthorLogin(ModelLogin.Login _login)
 		{
-			ModelLogin.Login _login = new ModelLogin.Login();
-			_login.UserName = "Test";
+			
+			byte[] text = Encoding.ASCII.GetBytes(new string('X', 10000));
+
+			// Use compress method.
+			byte[] compress = Compress(text);
+
+			// Write compressed data.
+			//File.WriteAllBytes("compress.gz", compress);
+
+			byte[] Decompressed = Decompress(compress);
 			return View("AddPost", "AuthorMainPage", _login);
 		}
 		/* for zip
@@ -77,40 +87,40 @@ namespace XamarinVisualStudio.Controllers
         File.WriteAllBytes("compress.gz", compress);
 
         byte[] Decompressed = Decompress(compress);**/
-		 public static byte[] Compress(byte[] raw)
-    {
-        using (MemoryStream memory = new MemoryStream())
-        {
-            using (GZipStream gzip = new GZipStream(memory, CompressionMode.Compress, true))
-            {
-                gzip.Write(raw, 0, raw.Length);
-            }
-            return memory.ToArray();
-        }
-    }
-    public static byte[] Decompress(byte[] gzip)
-    {
-        // Create a GZIP stream with decompression mode.
-        // ... Then create a buffer and write into while reading from the GZIP stream.
-        using (GZipStream stream = new GZipStream(new MemoryStream(gzip), CompressionMode.Decompress))
-        {
-            const int size = 4096;
-            byte[] buffer = new byte[size];
-            using (MemoryStream memory = new MemoryStream())
-            {
-                int count = 0;
-                do
-                {
-                    count = stream.Read(buffer, 0, size);
-                    if (count > 0)
-                    {
-                        memory.Write(buffer, 0, count);
-                    }
-                }
-                while (count > 0);
-                return memory.ToArray();
-            }
-        }
-    }
+		public static byte[] Compress(byte[] raw)
+		{
+			using (MemoryStream memory = new MemoryStream())
+			{
+				using (GZipStream gzip = new GZipStream(memory, CompressionMode.Compress, true))
+				{
+					gzip.Write(raw, 0, raw.Length);
+				}
+				return memory.ToArray();
+			}
+		}
+		public static byte[] Decompress(byte[] gzip)
+		{
+			// Create a GZIP stream with decompression mode.
+			// ... Then create a buffer and write into while reading from the GZIP stream.
+			using (GZipStream stream = new GZipStream(new MemoryStream(gzip), CompressionMode.Decompress))
+			{
+				const int size = 4096;
+				byte[] buffer = new byte[size];
+				using (MemoryStream memory = new MemoryStream())
+				{
+					int count = 0;
+					do
+					{
+						count = stream.Read(buffer, 0, size);
+						if (count > 0)
+						{
+							memory.Write(buffer, 0, count);
+						}
+					}
+					while (count > 0);
+					return memory.ToArray();
+				}
+			}
+		}
 	}
 }
